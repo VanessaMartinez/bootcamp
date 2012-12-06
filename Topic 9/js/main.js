a96a4e8e2304325b62d0c467e20b37431ec7404e
Vanessa Martinez <martinezvanessa_80@yahoo.com.ar>
2012-12-06 14:45:02 -0300

    librerias de 9

---
+++ Topic 9/js/main.js
@@ -0,0 +1,24 @@
+var worker = new Worker('js/task.js');
+worker.addEventListener('message', function(e) {
+  console.log('Worker said: ', e.data);
+}, false);
+
+worker.postMessage('Hello World'); // Send data to our worker.
+
+
+var connection = new WebSocket('ws://echo.websocket.org/');
+
+// When the connection is open, send some data to the server
+connection.onopen = function () {
+  connection.send('Testing web socket');
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
---
+++ Topic 9/js/task.js
@@ -0,0 +1,7 @@
+setInterval(function(){self.postMessage('worker s message');},60000);
+
+
+
+/*self.addEventListener('message', function(e) {
+  self.postMessage(e.data);
+}, false);*/
