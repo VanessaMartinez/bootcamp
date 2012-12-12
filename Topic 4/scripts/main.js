1f98b28cebd6b9c8ce1aecb14105b63d9c0a10b4
Vanessa Martinez <martinezvanessa_80@yahoo.com.ar>
2012-12-12 11:01:29 -0300

    correciones

--- Topic 4/scripts/main.js
+++ Topic 4/scripts/main.js
@@ -1,34 +1,23 @@
 require.config({
-    paths: {
-        'jquery': 'lib/jquery-1.7.1.min',
-        'jquerymobile': 'lib/jquery.mobile-1.1.0-rc.1.min'
-    },
-
-    shim: { 
-
-        'jquerymobile' : { 
-            deps: ['jquery'],
-            exports: 'jquerymobile'
-        }
-
+  paths: {
+    'jquery': 'lib/jquery-1.7.1.min',
+    'jquerymobile': 'lib/jquery.mobile-1.1.0-rc.1.min'
+  },
+  shim: { 
+    'jquerymobile' : { 
+      deps: ['jquery'],
+      exports: 'jquerymobile'
     }
-
+  }
 });
-
 require(['jquerymobile','director','movie'], function(jqm,director,movie) {
-  
-	$('body').attr("style", "visibility : visible");
-	   	
-        var alien = new movie();
-	var scott = new director('Ridley Scott');
-	
-	scott.set('quotes',['Cast is everthing','Do what..','Bla, bla']);
-
-	alien.set('director',scott);
-
-	alien.get('director').speak();
-        alien.get('director').randomquote();
-	
-	console.log('termino el main....');
+  $('body').attr("style", "visibility : visible");
+  var alien = new movie();
+  var scott = new director('Ridley Scott');
+  scott.set('quotes',['Cast is everthing','Do what..','Bla, bla']);
+  alien.set('director',scott);
+  alien.get('director').speak();
+  alien.get('director').randomquote();
+  console.log('end....');
 });
 
--- Topic 4/scripts/movie.js
+++ Topic 4/scripts/movie.js
@@ -1,18 +1,22 @@
 define( ['director'],function (director) {
-  	  	
- 		 // constructor
-   		 var movie = function () {};
-
-    		// prototype
-    		movie.prototype = {
-    		    constructor: movie,
-		    set : function(attr,value){this[attr]=value;},
-		    get : function(attr){return this[attr];},
-		    play : function(){return 'Playing '+ this.get('title') + ' ...'},
-		    stop : function(){return this.get('title')+' is stopped ...';}
-    		};
-
-    		// return module
-   		 return movie;
-	}
-);
+  // constructor
+  var movie = function () {};
+  // prototype
+  movie.prototype = {
+  constructor: movie,
+    set : function(attr,value){
+      this[attr]=value;
+    },
+    get : function(attr){
+      return this[attr];
+    },
+    play : function(){
+      return 'Playing '+ this.get('title') + ' ...';
+    },
+    stop : function(){
+      return this.get('title')+' is stopped ...';
+    }
+  };
+  // return module
+  return movie;
+});
